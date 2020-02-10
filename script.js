08bfaec243cd4a917e99fdc0783b77a5846c6384
David Martinez Carpena <dvmcarpena@pm.me>
2020-02-10 20:07:41 +0100

    New countdown page created

---
+++ script.js
@@ -0,0 +1,35 @@
+// Set the date we're counting down to
+const countDownDate = new Date("Feb 14, 2020 00:00:00").getTime();
+const target_id = "timer"
+
+function getDistance() {
+    var now = new Date().getTime();
+    return countDownDate - now;
+}
+
+function updateTimer() {
+    var distance = getDistance();
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById(target_id).innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+}
+
+function printResult() {
+    document.getElementById(target_id).innerHTML = "Today is the day, now is the time!";
+}
+
+var distance = getDistance();
+if (distance > 0) {
+    var updater = setInterval(updateTimer, 1000);
+
+    setTimeout(function() {
+        clearInterval(updater);
+        printResult();
+    }, distance);
+} else {
+    printResult();
+}
